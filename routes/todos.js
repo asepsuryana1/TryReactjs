8d2f38219c335abc0bca5ba106557843c711f05a
asepsuryana1 <asepsuryana1@gmail.com>
2020-11-22 14:57:47 +0700

    create roter get [] success

--- routes/todos.js
+++ routes/todos.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var Todo = require("../models/todo")
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
+Todo.find({},function (err, data) {
+  if (err) return res.status(500).json({err});
+  res.status(200).json(data)
+})
+})
 module.exports = router;
